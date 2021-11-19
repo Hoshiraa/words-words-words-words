49e836a774c60fca2fd4689f9811990248be7a0e
Hoshiraa <94674915+Hoshiraa@users.noreply.github.com>
2021-11-19 16:22:33 +0800

    Add files via upload

---
+++ pythonProject3/main.py
@@ -0,0 +1,45 @@
+def final_words():
+    name = input("Who are you? ")
+    if name == "a poet":
+        print("""What happens to a novel unread?
+What becomes of the string when life loose threads.
+Questions that keeps me up at night are left unanswered.
+A countdown of thoughts 'til the fall makes my insides scattered.""")
+        response = input("Do you like it? ")
+        if response == "yes":
+            print("Thank you.")
+        elif response == "no":
+            print("fair enough")
+        else:
+            print("ok")
+    elif name == "Kaycee":
+        print("""Yo, hello hello. Thanks for everything. No, really.
+I am a selfish, arrogant jerk. I've hurt u a lot and u deserve better.
+You'll make it one day.
+Sadness will never go but sadness is what makes happiness.
+I'll be proud of you, I already am. I'm writing poems for everyone so have a seat.""")
+        abayewan = input("You ready? ")
+        if abayewan == "yes":
+            print("""The noises, it hurts. My mouth is sealed shut while my organs disperse but there you were, smiling.
+You're a sun to my moon, like a sunny high noon cause with you, I'm at my highest.
+But I was too young to see that your sunshine-like glee hides that fact that you're hurting.
+Kaycee, my friend, my sibling. Take care of the treasure promised.""")
+            print("Godspeed, ate.")
+        else:
+            print("take your time and come back when you're ready.")
+
+    elif name == "Glai" or "Jess" or "Hannah" or "Althea":
+        print("""Sorry. I've always felt shitty when I told anyone my plans.
+I'm weak but u sure aren't.
+Jess' reliable, 
+Althea sure seem independent,
+Hannah seems like a crybaby but she sure isn't(lie)
+and Glai must be fierce, I've always been scared of her.
+Sorry. my reasons are too long to list so I've never talked about it but thanks for everything.
+This is one hell of a friend group. This is prolly the first real and genuine friend circle I've been in.
+I cry a lot and you guys did too, lmao. Idk what to say, I am out of words.
+I guess have a great life. Thank you. You were very fucking important to me. I love you all!! 
+""")
+
+
+final_words()
